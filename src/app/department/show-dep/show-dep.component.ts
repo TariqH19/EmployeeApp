c5fadba03f0c9a127feb370aec52cda1355dc935
Tariq Horan <N00202437@iadt.ie>
2023-07-27 11:35:57 +0100

    icons

--- src/app/department/show-dep/show-dep.component.ts
+++ src/app/department/show-dep/show-dep.component.ts
@@ -7,51 +7,77 @@ import { SharedService } from 'src/app/shared.service';
   styleUrls: ['./show-dep.component.css']
 })
 export class ShowDepComponent implements OnInit {
-
-  constructor(private service:SharedService){}
+  constructor(private service: SharedService) {}
 
   DepartmentList: any = [];
-
   ModalTitle: string | undefined;
   ActivateAddEditDepComp: boolean = false;
   dep: any;
 
-  ngOnInit(): void{
+  DepartmentIdFilter: string = "";
+  DepartmentNameFilter: string = "";
+  DepartmentListWithoutFilter: any = [];
+
+  ngOnInit(): void {
     this.refreshDepList();
   }
 
   addClick() {
     this.dep = {
       DepartmentId: 0,
-      DepartmentName:""
-    }
+      DepartmentName: ""
+    };
     this.ModalTitle = "Add Department";
     this.ActivateAddEditDepComp = true;
   }
 
-    closeClick() {
-      this.ActivateAddEditDepComp = false;
-      this.refreshDepList();
+  closeClick() {
+    this.ActivateAddEditDepComp = false;
+    this.refreshDepList();
   }
 
   editClick(item: any) {
     this.dep = item;
-    this.ModalTitle = "Edit Department"
+    this.ModalTitle = "Edit Department";
     this.ActivateAddEditDepComp = true;
   }
-  
-    deleteClick(item: any) {
-      if (confirm("Are you sure?")) {
+
+  deleteClick(item: any) {
+    if (confirm("Are you sure?")) {
       this.service.deleteDepartment(item.DepartmentId).subscribe(res => {
         alert(res.toString());
-              this.refreshDepList();
-    });
-}
-}
+        this.refreshDepList();
+      });
+    }
+  }
 
   refreshDepList() {
     this.service.getDeptList().subscribe(data => {
       this.DepartmentList = data;
-    })
+      this.DepartmentListWithoutFilter = data; // Update the unfiltered list
+    });
   }
+
+  FilterFn() {
+    const DepartmentIdFilter = this.DepartmentIdFilter.toLowerCase().trim();
+    const DepartmentNameFilter = this.DepartmentNameFilter.toLowerCase().trim();
+
+    this.DepartmentList = this.DepartmentListWithoutFilter.filter((el: any) => {
+      return (
+        el.DepartmentId.toString().toLowerCase().includes(DepartmentIdFilter) &&
+        el.DepartmentName.toString().toLowerCase().includes(DepartmentNameFilter)
+      );
+    });
+  }
+
+sortResult(prop: string, asc: boolean) {
+  this.DepartmentList = this.DepartmentListWithoutFilter.sort((a: any, b: any) => {
+    if (asc) {
+      return a[prop] > b[prop] ? 1 : a[prop] < b[prop] ? -1 : 0;
+    } else {
+      return b[prop] > a[prop] ? 1 : b[prop] < a[prop] ? -1 : 0;
+    }
+  });
+}
+
 }
